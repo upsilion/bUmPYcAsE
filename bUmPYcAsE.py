8ed07bfccd6a9738fb4ed1ff7f4a444a4edf5d53
upsilion <hardcorediggingdiger2@gmail.com>
2017-12-11 19:12:26 +1100

    fixed weighting for bumpines

--- bUmPYcAsE.py
+++ bUmPYcAsE.py
@@ -4,7 +4,7 @@ inputString = raw_input("Make this string bumpy: ")
 input = [x for x in inputString]
 x = [each for each in range(len(input))]
 
-for each in range(int(len(input)*0.8)):
+for each in range(int(len(input)*0.85)):
     index = random.choice(x)
     input[index] = input[index].upper()
     
