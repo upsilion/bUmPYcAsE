fff66f9450d59e76224a563de8ee055cfed5f467
upsilion <hardcorediggingdiger2@gmail.com>
2017-12-11 18:55:00 +1100

    first commit, very bad algorithm

---
+++ bUmPYcAsE.py
@@ -0,0 +1,11 @@
+import random
+
+inputString = raw_input("Make this string bumpy: ")
+input = [x for x in inputString]
+x = [each for each in range(len(input))]
+
+for each in range(int(len(input)*0.8)):
+    index = random.choice(x)
+    input[index] = input[index].upper()
+    
+print "".join(input)
